339d091012835c99e2a52001aac239f2c38b29c7
Maciej Chałapuk <maciej@chalapuk.pl>
2017-02-08 00:31:30 +0100

    + ttyrec.sh

---
+++ ttyrec.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# This script contains all commands typed when recording a demo with ttyrec.
+#
+# Copyright (c) Maciej Chałapuk
+# Released under MIT License
+
+cd ~/bin
+ls
+wget https://raw.githubusercontent.com/muroc/folders/master/folders.s://raw.githubusercontent.com/muroc/folders/master/folders.sh -O folders
+chmod +x folders
+folders ?
+
+ln -s folders repos
+
+cd ~/code
+repos list
+repos add *
+repos list
+
+repos run git status
+repos run git checkout master
+repos run git branch -u origin/master master
+repos run git pull
+repos -c run git push
+
